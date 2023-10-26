76e2e5c8546c3056efd7c0efb00760e56d3ed2b2
EunJI <83203198+ejalice@users.noreply.github.com>
2023-10-26 14:11:30 +0900

    [Build] #2 Project Setting

---
+++ mulmagi_SwiftUI/mulmagi_SwiftUI/mulmagi_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mulmagi_SwiftUIApp.swift
+//  mulmagi_SwiftUI
+//
+//  Created by eunji on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct mulmagi_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
