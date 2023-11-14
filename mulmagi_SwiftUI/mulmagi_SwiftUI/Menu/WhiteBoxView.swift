5c57ba0fcee594e3cc58f984fa6dfaed2c89efac
EunJI <83203198+ejalice@users.noreply.github.com>
2023-11-14 13:26:11 +0900

    [Feat] #8 Change Scroll

--- mulmagi_SwiftUI/mulmagi_SwiftUI/Menu/WhiteBoxView.swift
+++ mulmagi_SwiftUI/mulmagi_SwiftUI/Menu/WhiteBoxView.swift
@@ -11,42 +11,44 @@ struct WhiteBoxView: View {
     var menuName: String
     
     var body: some View {
-        VStack(alignment: .leading) {
-            if titleText(menuName: menuName) {
-                VStack (alignment: .leading) {
-                    HStack {
-                        Text("[공지]")
-                            .foregroundColor(.mainBlue)
-                            .padding(.trailing, 10)
+        ScrollView {
+            VStack(alignment: .leading) {
+                if titleText(menuName: menuName) {
+                    VStack (alignment: .leading) {
+                        HStack {
+                            Text("[공지]")
+                                .foregroundColor(.mainBlue)
+                                .padding(.trailing, 10)
+                            
+                            // TODO: API로 제목 가져오기
+                            Text("오류로 인한 포인트 일괄 지급")
+                                .foregroundColor(.black)
+                            
+                        }
+                        .font(.medium15)
+                        .padding(.bottom, 1)
                         
-                        // TODO: API로 제목 가져오기
-                        Text("오류로 인한 포인트 일괄 지급")
-                            .foregroundColor(.black)
-                        
+                        Text("2023. 07. 28")
+                            .font(.light10)
+                            .foregroundColor(.midGray)
+                            .padding(.bottom, 2)
                     }
-                    .font(.medium15)
-                    .padding(.bottom, 1)
                     
-                    Text("2023. 07. 28")
-                        .font(.light10)
-                        .foregroundColor(.midGray)
-                        .padding(.bottom, 2)
+                    Divider()
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
                 }
                 
-                Divider()
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
+                Text("오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내")
+                    .font(.light13)
+                    .foregroundColor(.black)
+                    .tracking(-0.41)
+                    .lineSpacing(7)
+                
             }
-            
-            Text("오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 오류로 인한 포인트 일괄 지급 안내 오류로 인한 포인트 일괄 지급 안내")
-                .font(.light13)
-                .foregroundColor(.black)
-                .tracking(-0.41)
-                .lineSpacing(7)
-            
+            .padding(EdgeInsets(top: 15, leading: 18, bottom: 15, trailing: 18))
+            .background(.white)
+            .cornerRadius(14)
         }
-        .padding(EdgeInsets(top: 25, leading: 18, bottom: 15, trailing: 18))
-        .background(.white)
-        .cornerRadius(14)
     }
 }
 
