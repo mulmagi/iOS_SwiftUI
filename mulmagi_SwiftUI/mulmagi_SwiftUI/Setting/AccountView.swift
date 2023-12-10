8d858f2d36ac4f9e865140af0b5c04310ffa29c9
EunJI <83203198+ejalice@users.noreply.github.com>
2023-12-10 14:19:01 +0900

    [Feat] #30 Create Logout and Withdraw

--- mulmagi_SwiftUI/mulmagi_SwiftUI/Setting/AccountView.swift
+++ mulmagi_SwiftUI/mulmagi_SwiftUI/Setting/AccountView.swift
@@ -8,17 +8,63 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var showLogoutAlert: Bool = false
+    @State var showWidthdrawAlert: Bool = false
     
     var body: some View {
         NavigationView {
-            List {
-                HStack {
-                    Text("버전 정보")
-                    Spacer()
-                    Text(appVersion())
+            VStack {
+                List {
+                    HStack {
+                        Text("버전 정보")
+                            .font(.medium15)
+                        Spacer()
+                        Text(appVersion())
+                            .font(.light13)
+                    }
+                    
+                    NavigationLink("이용약관", destination: TOSView())
+                        .font(.medium15)
+                    
+                    Section(header: Text("계정")) {
+                        NavigationLink("전화번호 수정", destination: TOSView())
+                        Text("로그아웃")
+                            .onTapGesture {
+                                showLogoutAlert = true
+                            }
+                            .alert(isPresented: $showLogoutAlert) {
+                                let logoutButton = Alert.Button.default(Text("로그아웃")) {
+                                    // TODO: Logout API
+                                }
+                                
+                                return Alert(title: Text("로그아웃"),
+                                      message: Text("로그아웃 하시겠습니까?."),
+                                      primaryButton: logoutButton,
+                                      secondaryButton: .cancel(Text("취소")))
+                            }
+                        
+                        Text("회원탈퇴")
+                            .onTapGesture {
+                                showWidthdrawAlert = true
+                            }
+                            .alert (isPresented: $showWidthdrawAlert) {
+                                let withdrawButton = Alert.Button.default(Text("회원탈퇴")) {
+                                    // TODO: withdraw API
+                                }
+                                
+                                return Alert(title: Text("회원탈퇴"),
+                                             message: Text("탈퇴 하시겠습니까?"),
+                                             primaryButton: withdrawButton,
+                                             secondaryButton: .cancel(Text("취소")))
+                            }
+                    }
                 }
+                .padding(.bottom, 32)
+                .listStyle(InsetGroupedListStyle())
             }
         }
+        .foregroundColor(.darkNavy)
+            
     }
 }
 
---
+++ mulmagi_SwiftUI/mulmagi_SwiftUI/Setting/TOSView.swift
@@ -0,0 +1,20 @@
+//
+//  TOSView.swift
+//  mulmagi_SwiftUI
+//
+//  Created by eunji on 2023/12/10.
+//
+
+import SwiftUI
+
+struct TOSView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TOSView_Previews: PreviewProvider {
+    static var previews: some View {
+        TOSView()
+    }
+}
